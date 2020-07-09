1d35c317073de2bc5ecdcb2f943caec0c0c6c0db
salaandora <50085010+salaandora@users.noreply.github.com>
2020-07-08 22:50:03 -0400

    Add files via upload

---
+++ proof 3.py
@@ -0,0 +1,8 @@
+#Defines two sets each with three elements
+a = ['b', 'c', 'd']
+e = ['f', 'g'. 'h']
+#Creates a union of a, b within set a
+for let in e:
+    a += let
+#Prints length of set a, equivalent to the union of a, b
+print (len(a))
